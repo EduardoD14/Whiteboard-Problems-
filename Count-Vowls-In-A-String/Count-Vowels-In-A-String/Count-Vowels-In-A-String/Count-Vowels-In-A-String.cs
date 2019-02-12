f24f09ca5d09a25264880cccac0f202a6957f44f
Eduardo Davila <DAVILAE1@my.erau.edu>
2019-02-12 10:23:33 -0800

    Added Count Voewls In a string

---
+++ Count-Vowls-In-A-String/Count-Vowels-In-A-String/Count-Vowels-In-A-String/Count-Vowels-In-A-String.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Count_Vowels_In_A_String
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the sentence to count voewls for: ");
+            string sentence = Console.ReadLine();
+            Console.WriteLine(CountVowels(sentence));
+            Console.ReadKey();
+        }
+        public static int CountVowels(string sentence)
+        {
+            int total = 0;
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                if
+                    (sentence[i] == 'a' || sentence[i] == 'e' || sentence[i] == 'i' ||
+                    sentence[i] == 'o' || sentence[i] == 'u')
+                {
+                    total++;
+                };
+
+            }
+            return total;
+        }
+    }
+}
