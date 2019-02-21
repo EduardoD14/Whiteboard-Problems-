dd8cb9a212c9bc60110b008f2acbd93c41d241b9
Eduardo Davila <eduardo.davila.nu@gmail.com>
2019-02-21 09:38:22 -0800

    add

---
+++ ReturnSumOfDigits/ReturnSumOfDigits/ReturnSumOfDigits.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ReturnSumOfDigits
+{
+    public class ReturnSumOfDigits
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number please:");
+            //Console.WriteLine(SumDig(Convert.ToInt32(Console.ReadLine())));
+            Console.WriteLine(SingleSumDig(Convert.ToInt32(Console.ReadLine())));
+            Console.ReadKey();
+        }
+        //The code below adds up all of the digits in a number.        
+        public static int SumDig(int x)
+        {
+            int sum = 0;
+            while (x > 0)
+            {
+                sum += x % 10;
+                x /= 10;
+            }
+            return sum;
+        }
+        //The code below adds up all of the digits in a number and if the resulting number has
+        //more than one digit we will cycle through it again until the result is a single digit.
+        public static int SingleSumDig(int x)
+        {
+            int sum = 0;
+            while (x > 0)
+            {
+                sum += x % 10;
+                x /= 10;
+            }
+            return sum > 9 ? SumDig(sum) : sum;
+        }
+    }
+}
---
+++ ReturnSumOfDigits/ReturnSumOfDigitsTests/ReturnSumOfDigitsTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ReturnSumOfDigits;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReturnSumOfDigits.Tests
+{
+    [TestClass()]
+    public class ReturnSumOfDigitsTests
+    {
+        [TestMethod()]
+        public void SumDigTest()
+        {        
+            Assert.AreEqual(1, ReturnSumOfDigits.SingleSumDig(1234));
+        }
+         [TestMethod()]
+        public void SumDigTest1()
+        {   
+            Assert.AreEqual(6, ReturnSumOfDigits.SingleSumDig(12345));
+        }
+    }          
+}
